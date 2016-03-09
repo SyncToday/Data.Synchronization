a9b9c6514f8bb86c917ab57e3895b0c02cf22d45
David Podhola <david.podhola@naseukoly.cz>
2016-03-09 14:53:30 +0100

    initial commit

---
+++ docs/content/tutorial.fsx
@@ -0,0 +1,19 @@
+(*** hide ***)
+// This block of code is omitted in the generated HTML documentation. Use 
+// it to define helpers that you do not want to show in the documentation.
+#I "../../bin"
+
+(**
+Introducing your project
+========================
+
+Say more
+
+*)
+#r "SyncToday.Data.Synchronization.dll"
+open SyncToday.Data.Synchronization
+
+Library.hello 0
+(**
+Some more info
+*)
