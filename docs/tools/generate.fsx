d09818f2c3f3bec9bb9573278aa69661be5cf239
David Podhola <david@podhola.net>
2016-03-09 22:07:35 +0100

    GA in template
    website is in the root (we have CNAME)

--- docs/tools/generate.fsx
+++ docs/tools/generate.fsx
@@ -11,7 +11,7 @@
 // (This is the original behaviour of ProjectScaffold prior to multi project support)
 let referenceBinaries = []
 // Web site location for the generated documentation
-let website = "/SyncToday.Data.Synchronization"
+let website = "/"
 
 let githubLink = "http://github.com/SyncToday/Data.Synchronization"
 
