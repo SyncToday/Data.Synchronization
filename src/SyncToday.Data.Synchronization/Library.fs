713550e0e793e550701ecc1ffd54aaf5bc85460d
David Podhola <david.podhola@naseukoly.cz>
2016-03-09 16:37:31 +0100

    finally renamed + Tests added

--- src/SyncToday.Data.Synchronization/Library.fs
+++ src/SyncToday.Data.Synchronization/Library.fs
@@ -7,10 +7,41 @@ namespace SyncToday.Data.Synchronization
 ///     let h = Library.hello 1
 ///     printfn "%d" h
 ///
-module Library = 
+module Synchronizer = 
   
   /// Returns 42
   ///
   /// ## Parameters
   ///  - `num` - whatever
-  let hello num = 42
+  let synchronize 
+    ( mapped: ('system1Id*'system2Id) seq ) (system1Ids:'system1Id seq) (system2Ids:'system2Id seq) 
+    (get1:'system1Id->'system1) (get2:'system2Id->'system2)
+    (createIn1:'system2->'system1Id) (createIn2:'system1->'system2Id) 
+    (updateIn1:'system1->unit) (updateIn2:'system2->unit) 
+    (compareAndUpdate:'system1*'system2*('system1->unit)*('system2->unit)->unit) =
+
+    let getDifference ( mapped: ('system1Id*'system2Id) seq ) ( chooser : ('system1Id*'system2Id) -> 'systemId ) ( all:'systemId seq ) =
+      set (all |> Seq.toArray) - set (mapped |> Seq.map( chooser ) |> Seq.toArray) |> Set.toSeq
+    let createIn1Ids = 
+      getDifference mapped  ( fun (_,p) -> p ) system2Ids
+    let createIn2Ids = 
+      getDifference mapped  ( fun (p,_) -> p ) system1Ids
+    let mappedExisting = 
+      mapped 
+      |> Seq.filter( 
+        fun (id1,id2) -> 
+          system1Ids |> Seq.exists ( fun p->p=id1 ) &&
+          system2Ids |> Seq.exists ( fun p->p=id2 ) 
+      )
+    let createInOther (ids: 'systemId seq) (get:'systemId->'a) (create:'a->'systemId' ) =
+      ids |> Seq.map( fun id -> (id,get(id)) ) |> Seq.map( fun (id,entity) -> (id,create(entity)) )
+    let append1 = 
+      createInOther createIn1Ids get2 createIn1 |> Seq.map ( fun(a,b) -> (b,a) ) |> Seq.toList
+    let append2 = 
+      createInOther createIn2Ids get1 createIn2 |> Seq.toList
+
+    mappedExisting |> Seq.iter ( fun (id1,id2) -> compareAndUpdate(get1(id1),get2(id2),updateIn1,updateIn2) ) 
+
+    ( mapped |> Seq.toList) @ append1 @ append2 |> List.toSeq
+
+    
--- tests/SyncToday.Data.Synchronization.Tests/Tests.fs
+++ tests/SyncToday.Data.Synchronization.Tests/Tests.fs
@@ -1,10 +1,107 @@
-module SyncToday.Data.Synchronization.Tests
+module SyncToday.Data.Synchronizer.Tests
 
 open SyncToday.Data.Synchronization
 open NUnit.Framework
 
+let dummy a = a
+let nothing _ = ()
+let notcalled _ = failwith "create should not be called"
+let comparenotcalled (_,_,_,_) = failwith "compare should not be called"
+
 [<Test>]
-let ``hello returns 42`` () =
-  let result = Library.hello 42
-  printfn "%i" result
-  Assert.AreEqual(42,result)
+let ``1-empty sync does nothing`` () =
+  let result = Synchronizer.synchronize [] [] [] dummy dummy notcalled notcalled notcalled notcalled comparenotcalled
+  Assert.IsNotNull(result)
+  Assert.IsEmpty( result )
+
+[<Test>]
+let ``2-no data in system means we keep the mapping as it was before`` () =
+  let result = Synchronizer.synchronize [|("2","X")|] [] [] dummy dummy notcalled notcalled notcalled notcalled comparenotcalled
+  Assert.IsNotNull( result )
+  Assert.IsNotEmpty( result )
+  Assert.AreEqual( ("2","X"), result |> Seq.head )
+
+[<Test>]
+let ``3-same data in system means we keep the mapping as it was before`` () =
+  let mutable compareCalled = false
+  let result = Synchronizer.synchronize [|("2","X")|] [|"2"|] [|"X"|] dummy dummy notcalled notcalled notcalled notcalled (fun (_,_,_,_) -> compareCalled <-true )
+  Assert.IsNotNull( result )
+  Assert.IsNotEmpty( result )
+  Assert.AreEqual( ("2","X"), result |> Seq.head )
+  Assert.IsTrue(compareCalled)
+
+[<Test>]
+let ``4-pseudoreal test`` () =
+  let mapped = [| ("2","X"); ("3","A"); ("5","J"); ("7","B") |]
+  let system1 = [| "2";"3";"7";"9" |]
+  let system2 = [| "A";"B";"J";"P";"X" |]
+  let compareAndUpdate (system1:'system1,system2:'system2,update1:'system1->unit,update2:'system2->unit) : unit =
+    ()
+      
+  let result = Synchronizer.synchronize mapped system1 system2 dummy dummy dummy dummy nothing nothing compareAndUpdate
+  Assert.IsNotNull( result )
+  Assert.IsNotEmpty( result )
+  Assert.AreEqual( [("P","P");("9","9")] |> Seq.append mapped, result )
+
+[<Test>]
+let ``5-same data with different types in system means we keep the mapping as it was before`` () =
+  let mutable compareCalled = false
+  let result = Synchronizer.synchronize [|(2,"X")|] [|2|] [|"X"|] dummy dummy notcalled notcalled notcalled notcalled (fun (_,_,_,_) -> compareCalled <-true )
+  Assert.IsNotNull( result )
+  Assert.IsNotEmpty( result )
+  Assert.AreEqual( (2,"X"), result |> Seq.head )
+  Assert.IsTrue(compareCalled)
+
+[<Test>]
+let ``6-pseudoreal test with different types`` () =
+  let dummySI (a:string) = int(a.[0])
+  let dummyIS (a:int) = a.ToString()
+  let mapped = [| (2,"X"); (3,"A"); (5,"J"); (7,"B") |]
+  let system1 = [| 2;3;7;9 |]
+  let system2 = [| "A";"B";"J";"P";"X" |]
+  let compareAndUpdate (system1:'system1,system2:'system2,update1:'system1->unit,update2:'system2->unit) : unit =
+    ()
+      
+  let result = Synchronizer.synchronize mapped system1 system2 dummy dummy dummySI dummyIS nothing nothing compareAndUpdate
+  Assert.IsNotNull( result )
+  Assert.IsNotEmpty( result )
+  Assert.AreEqual( [(80,"P");(9,"9")] |> Seq.append mapped, result )
+
+[<Test>]
+let ``7-empty mapping, data in systems means create and map`` () =
+  let mutable dummySICalled = false
+  let mutable dummyISCalled = false
+  let dummySI (a:string) = 
+    if dummySICalled then failwith "dummySICalled"
+    dummySICalled <- true
+    int(a.[0])
+  let dummyIS (a:int) = 
+    if dummyISCalled then failwith "dummyISCalled"
+    dummyISCalled <- true
+    char(a).ToString()
+  let mapped = [||]
+  let system1 = [| int('A') |]
+  let system2 = [| "B" |]
+  let compareAndUpdate (system1:'system1,system2:'system2,update1:'system1->unit,update2:'system2->unit) : unit =
+    ()
+      
+  let result = Synchronizer.synchronize mapped system1 system2 dummy dummy dummySI dummyIS nothing nothing compareAndUpdate
+  Assert.IsNotNull( result )
+  Assert.IsNotEmpty( result )
+  Assert.AreEqual( mapped |> Seq.append [(int('B'),"B");(int('A'),"A")], result )
+
+[<Test>]
+let ``8-twice same means nothing`` () =
+  let dummySI (a:string) = int(a.[0])
+  let dummyIS (a:int) = char(a).ToString()
+  let mapped = [||]
+  let system1 = [| int('A') |]
+  let system2 = [| "B" |]
+  let compareAndUpdate (system1:'system1,system2:'system2,update1:'system1->unit,update2:'system2->unit) : unit =
+    ()
+      
+  let result = Synchronizer.synchronize mapped system1 system2 dummy dummy dummySI dummyIS nothing nothing compareAndUpdate
+  let result = Synchronizer.synchronize result [| int('A');int('B') |] [| "A";"B" |] dummy dummy dummySI dummyIS nothing nothing compareAndUpdate
+  Assert.IsNotNull( result )
+  Assert.IsNotEmpty( result )
+  Assert.AreEqual( [(int('B'),"B");(int('A'),"A")] |> Seq.append mapped, result )
