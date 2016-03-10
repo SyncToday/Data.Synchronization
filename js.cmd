6708589c5f154a19a9186da4fde5d3ad1fa66e42
David Podhola <david@podhola.net>
2016-03-10 23:33:30 +0100

    no need to have js.cmd anymore

--- js.cmd
+++
@@ -1,2 +0,0 @@
-rem node paket-files/js/fsprojects/Fable/build/fable --projFile src/SyncToday.Data.Synchronization/SyncToday.Data.Synchronization.fsproj --outDir ../../bin/js
-node paket-files/js/fsprojects/Fable/build/fable --projFile src/SyncToday.Data.Synchronization/Script.fsx --outDir ../../bin/js --env node
