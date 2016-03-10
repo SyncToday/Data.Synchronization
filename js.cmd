23aa7416cbdc96ffeb9176f913448893f8f117ea
David Podhola <david@podhola.net>
2016-03-10 19:17:58 +0100

    compiling to JS as for node

--- js.cmd
+++ js.cmd
@@ -1,2 +1,2 @@
 rem node paket-files/js/fsprojects/Fable/build/fable --projFile src/SyncToday.Data.Synchronization/SyncToday.Data.Synchronization.fsproj --outDir ../../bin/js
-node paket-files/js/fsprojects/Fable/build/fable --projFile src/SyncToday.Data.Synchronization/Script.fsx --outDir ../../bin/js
+node paket-files/js/fsprojects/Fable/build/fable --projFile src/SyncToday.Data.Synchronization/Script.fsx --outDir ../../bin/js --env node
